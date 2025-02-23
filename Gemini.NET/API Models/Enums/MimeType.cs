fe78247458ffae2eb9386859e8b68acfb4ac1fce
PHAN Xuan Quang <20521008@ms.uit.edu.vn>
2025-02-23 13:22:07 +0700

    update

--- Gemini.NET/API Models/Enums/MimeType.cs
+++ Gemini.NET/API Models/Enums/MimeType.cs
@@ -5,8 +5,32 @@ namespace Gemini.NET.API_Models.Enums
     /// <summary>
     /// The media type of the file specified in the Inline Data
     /// </summary>
-    public enum MimeType : sbyte
+    public enum MimeType
     {
+        /// <summary>
+        /// MIME type for PDF documents.
+        /// </summary>
+        [Description("application/pdf")]
+        ApplicationPdf,
+
+        /// <summary>
+        /// MIME type for MPEG audio.
+        /// </summary>
+        [Description("audio/mpeg")]
+        AudioMpeg,
+
+        /// <summary>
+        /// MIME type for MP3 audio.
+        /// </summary>
+        [Description("audio/mp3")]
+        AudioMp3,
+
+        /// <summary>
+        /// MIME type for WAV audio.
+        /// </summary>
+        [Description("audio/wav")]
+        AudioWav,
+
         /// <summary>
         /// MIME type for PNG images.
         /// </summary>
@@ -14,27 +38,69 @@ namespace Gemini.NET.API_Models.Enums
         ImagePng,
 
         /// <summary>
-        /// JPEG images.
+        /// MIME type for JPEG images.
         /// </summary>
         [Description("image/jpeg")]
         ImageJpeg,
 
         /// <summary>
-        /// WEBP images.
+        /// MIME type for WEBP images.
         /// </summary>
         [Description("image/webp")]
         ImageWebp,
 
         /// <summary>
-        /// HEIC images.
+        /// MIME type for plain text.
         /// </summary>
-        [Description("image/heic")]
-        ImageHeic,
+        [Description("text/plain")]
+        TextPlain,
 
         /// <summary>
-        /// HEIF images.
+        /// MIME type for MOV video.
         /// </summary>
-        [Description("image/heif")]
-        ImageHeif,
+        [Description("video/mov")]
+        VideoMov,
+
+        /// <summary>
+        /// MIME type for MPEG video.
+        /// </summary>
+        [Description("video/mpeg")]
+        VideoMpeg,
+
+        /// <summary>
+        /// MIME type for MP4 video.
+        /// </summary>
+        [Description("video/mp4")]
+        VideoMp4,
+
+        /// <summary>
+        /// MIME type for MPG video.
+        /// </summary>
+        [Description("video/mpg")]
+        VideoMpg,
+
+        /// <summary>
+        /// MIME type for AVI video.
+        /// </summary>
+        [Description("video/avi")]
+        VideoAvi,
+
+        /// <summary>
+        /// MIME type for WMV video.
+        /// </summary>
+        [Description("video/wmv")]
+        VideoWmv,
+
+        /// <summary>
+        /// MIME type for MPEGPS video.
+        /// </summary>
+        [Description("video/mpegps")]
+        VideoMpegps,
+
+        /// <summary>
+        /// MIME type for FLV video.
+        /// </summary>
+        [Description("video/flv")]
+        VideoFlv
     }
 }
